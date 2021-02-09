054080374ce33716d2befa692534ba3f65caf8d8
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2021-02-08 22:49:57 -0300

    Ajustes finais em CannonView acrescetando rotinas de controle de toque na tela, desenhar componentes e colisões. Ajuste no xml da view de fragment bem como codificação dos alvos _Target_ e Bloqueios _Blocker_

--- app/src/main/java/br/com/progiv/cannongame/CannonView.java
+++ app/src/main/java/br/com/progiv/cannongame/CannonView.java
@@ -2,16 +2,23 @@ package br.com.progiv.cannongame;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.media.AudioAttributes;
 import android.media.SoundPool;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.SparseIntArray;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -190,7 +197,7 @@ public class CannonView extends SurfaceView
     }
     //chamado repetidadmente por CannonThread para atualizar os elementos do jogo
     private void updatePositions(double elapsedTimeMS){
-        double interval = elapsedTimeMS * 1000.0; //converte em segundos
+        double interval = elapsedTimeMS / 1000.0; //converte em segundos
         //atualizar a posição da bala, se estiver na tela
         if(cannon.getCannonBall() != null)
             cannon.getCannonBall().update(interval);
@@ -207,13 +214,13 @@ public class CannonView extends SurfaceView
             timeLeft = 0.0;
             gameOver = true;// o jogo terminou
             cannonThread.setRunning(false); //termina a thread
-            showGameOverDialog("You lose!"); //mostrar a caixa de diálogo
+            showGameOverDialog(R.string.lose); //mostrar a caixa de diálogo
         }
 
         //se todas as peças foram atingidas
         if(targets.isEmpty()){
             cannonThread.setRunning(false);
-            showGameOverDialog("You win!");
+            showGameOverDialog(R.string.win);
             gameOver = true;
         }
     }
@@ -226,7 +233,198 @@ public class CannonView extends SurfaceView
         //calcular a distância do toque a partir do centro
         double centerMinusY = (screenHeight / 2 - touchPoint.y);
         double angle = 0; //inicializa o ângulo com 0
+        //calcular o ângulo do cano em relação à hohrizontal
+        angle = Math.atan2(touchPoint.x, centerMinusY);
+        //apontar o cano para o ponto onde a tela foi tocada
+        cannon.align(angle);
+        //disparar a bala se ainda não houver uma na tela
+        if(cannon.getCannonBall() == null || !cannon.getCannonBall().isOnScreen()){
+            cannon.fireCannonBall();
+            ++shotsFired;
+        }
+    }
 
+    //exibir um componente AlertDialog quando o jogo terminar:
+    private void showGameOverDialog(final int messageId){
+        //ajustar depois:
+        //------------------------
+        activity.runOnUiThread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        showSystemBars();
+                        dialogDisplayed = true;
+                        //gameResult.setCancelable(false);//modal dialog
+                        //gameResult.show(activity.getFragmentManager(),"results");
+                    }
+                }
+        );
+    }
+
+    //desnha o jogo no objeto Canvas:
+    public void drawGameElement(Canvas canvas){
+        //limpar o pano de fundo
+        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), backgroundPaint);
+        //exibir o tempo restante:
+        canvas.drawText(getResources().getString(R.string.time_remaining_format, timeLeft), 50, 100, textPaint);
+        //desenhar o canhão
+        cannon.draw(canvas);
+        //desenha os elementos do jogo:
+        if(cannon.getCannonBall() != null && cannon.getCannonBall().isOnScreen()){
+            cannon.getCannonBall().draw(canvas);
+        }
+        //desenha a barreira:
+        blocker.draw(canvas);
+        //desenha os alvos:
+        for(GameElement target : targets){
+            target.draw(canvas);
+        }
+    }
+    //teste de colisão
+    public void testForCollision(){
+        //remove quaisquer alvos em que a bala colida:
+        if(cannon.getCannonBall() != null && cannon.getCannonBall().isOnScreen()){
+           for(int n = 0; n < targets.size(); n++){
+               if(cannon.getCannonBall().collidesWith(targets.get(n))){
+                   targets.get(n).playSound();//reproduz o som de acerto no alvo
+                   timeLeft += targets.get(n).getHitReward(); //adiciona ao tempo restante o tempo de recompensa
+                   cannon.removeCannonBall();// remover a bala do jogo
+                   targets.remove(n);//remove o alvo do array
+                   --n;
+                   break;
+               }
+           }
+        }else{
+            cannon.removeCannonBall();
+        }
+        //verificar se a bala colide com a barreira:
+        if(cannon.getCannonBall() != null && cannon.getCannonBall().collidesWith(blocker)){
+            blocker.playSound();
+            //inverter a diração da bala
+            cannon.getCannonBall().reverseVelocityX();
+            //subtrair o tempo pela penalidade:
+            timeLeft -= blocker.getMissPenalty();
+        }
+    }
+    //interrompe o jogo chamado pelo método onPause de CannonGameFragment
+    public  void stopGame(){
+        if(cannonThread != null)
+            cannonThread.setRunning(false);
+    }
+    //libera recursos: chamado pelo método onDestroy de view CannonGame
+    public void releaseResources(){
+        soundPool.release();//libera todos os recursos usados pelo soudpool
+        soundPool = null;
+    }
+
+    //chamado quando o tamanho da superfície muda
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) { }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        if(!dialogDisplayed){
+            newGame();
+            cannonThread = new CannonThread(holder);
+            cannonThread.setRunning(true);
+            cannonThread.start();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+        boolean retry = true;
+        cannonThread.setRunning(false);
+        while(retry){
+            try{
+                cannonThread.join();//espera cannonThread termine
+                retry = false;
+            }catch (InterruptedException e){
+                Log.e(TAG, "Thread interrupted",e);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //obter o valor int representado o tipo de ação que causou esse evento
+        int action = event.getAction();
+        //o usuário tocou na tela ou arrastou o dedo?
+        if(action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE){
+            alignAndFireCannonBall(event);
+        }
+        return true;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        screenWidth = w; //armazena a largura do CannonView
+        screenHeight = h; //Armazena a altura
+        //configurar a propriedade do texto:
+        textPaint.setTextSize((int)(TEXT_SIZE_PERCENT * screenHeight));
+        textPaint.setAntiAlias(true); //suaviza o texto
+    }
+
+    //subclasse de thread para controlar o loop do jogo
+    private class CannonThread extends Thread {
+        private SurfaceHolder surfaceHolder;// para manipular o canvas
+        private boolean threadIsRunning = true; //executando por padrão
+
+        //inicializar o surfaceHolder
+        public CannonThread(SurfaceHolder holder){
+            surfaceHolder = holder;
+            setName("CannonThread");
+        }
+        //altera o estado de execução
+        public void setRunning(boolean running){ threadIsRunning = running;}
+
+        @Override
+        public void run() {
+            Canvas canvas = null;
+            long previousFrameTime = System.currentTimeMillis();
+            while (threadIsRunning){
+                try{
+                    canvas = surfaceHolder.lockCanvas(null);
+                    //bloquear o surfaceHolder para desenhar
+                    synchronized (surfaceHolder){
+                        long currentTime = System.currentTimeMillis();
+                        double elapsedTimeMS = currentTime - previousFrameTime;
+                        totalElapsedTime += elapsedTimeMS /1000.0;
+                        updatePositions(elapsedTimeMS);
+                        testForCollision();
+                        drawGameElement(canvas);//desenha usando o canvas
+                        previousFrameTime = currentTime;
+                    }
+                }finally {
+                    if(canvas != null)
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+        }
+    }
+
+    //oculta a barra de sistema e a barra de aplicativo
+    private void hideSystemBars(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_IMMERSIVE
+            );
+    }
+
+    //mostra as barras de sistema e a barra de aplicativo:
+    private void showSystemBars(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            );
     }
 
 }
